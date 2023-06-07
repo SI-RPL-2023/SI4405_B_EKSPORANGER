bb8674ba266fd0bcd172072d980e3598101a6403
fiftyshadess <donnalolitaharahap@gmail.com>
2023-06-07 12:04:19 +0700

    update controlleruser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Pick_up;
 use App\Models\Product;
+use App\Models\Request as ModelsRequest;
+use App\Models\Ship;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
@@ -13,13 +16,81 @@ class UserController extends Controller
     public function Home(){
         return view('pages.home');
     }
+    public function pesanan(){
+        $listTransaksi = Transaction::all();
+        return view('pages.Pesanan', compact('listTransaksi'));
+    }
+    public function detailpesanan($id){
+        $detailpesanan = Transaction::find($id);
+        return view('pages.detailpesanan', compact('detailpesanan'));
+    }
+    public function statusPesanan(){
+        return view('pages.statusPesanan');
+    }
+    public function updateStatusPesanan(Request $request, $id){
+        $updateStatusPesanan = Transaction::find($id);
+        $updateStatusPesanan->update($request->except('_token'));
+        if($updateStatusPesanan){
+            return redirect('/pesanan');
+        } else {
+            Session::flash('status', 'failed');
+            Session::flash('message', 'Pemesanan gagal');
+            return redirect('/pesanan/statusPesanan/'.$id);
+        }
+    }
+    public function pickup(){
+        $listPickup = Transaction::all();
+        return view('pages.pickup', compact('listPickup'));
+    }
     public function addProduct(){
         return view('pages.eksportir.TambahProduk');
     }
-    public function requestPickup(){
-        return view('pages.requestPickup');
+    public function daftarPickup(){
+        $daftarPickup = Pick_up::all();
+        return view('pages.daftarPickup', compact('daftarPickup'));
     }
-    public function requestPickupMade(Request $request){
+    public function requestPickup($id){
+        $requestPickup = Transaction::find($id);
+        return view('pages.requestPickup', compact('requestPickup'));
+    }
+    public function revisi(Request $request){
+        $cari = $request->cari;
+        $listProduct = Product::where('nama_barang', 'LIKE', '%'. $cari. '%')
+        ->where('status_kelayakan', 'Ditolak (Mohon perbaiki deskripsi barang / foto)')
+        ->paginate(15);
+        return view('pages.permintaanrevisi', compact(['listProduct']));
+    }
+    public function updateProduk($id){
+        $detailProduct = Product::find($id);
+        return view('pages.eksportir.updateProduk', compact(['detailProduct']));
+    }
+    public function revisiProduk(Request $request, $id){
+        $revisiProduk = Product::find($id);
+        if ($request->hasFile('foto_barang')) {
+            $ekstensi = $request->file('foto_barang')->clientExtension();
+            $nama = $request->nama_barang.'-'.now()->timestamp.'.'.$ekstensi;
+            $request->file('foto_barang')->storeAs('images', $nama);
+            $request['foto_barang'] = $nama;
+            $revisiProduk->foto_barang = $nama;
+        }
+        $revisiProduk->update([
+            'nama_barang' => $request->nama_barang,
+            'deskripsi_barang' => $request->deskripsi_barang,
+            'status_kelayakan' => 'Menunggu konfirmasi',
+        ]);
+
+        if($revisiProduk){
+        Session::flash('status', 'success');
+        Session::flash('message', 'Proses revisi selesai');
+        return redirect('updateProduk/'.$id);
+        } else {
+            Session::flash('status', 'failed');
+            Session::flash('message', 'Proses revisi selesai');
+            return redirect('updateProduk/'.$id);
+        }
+    }
+    public function requestPickupMade(Request $request, $id){
+        $updateStatusPemesanan = Transaction::find($id);
         $requestPickup = Pick_up::create([
             'nama_toko' => $request->nama_toko,
             'nama_produk' => $request->nama_produk,
@@ -31,14 +102,17 @@ class UserController extends Controller
             'detail_produk' => $request->detail_produk,
             'status_pickup' => 'Menunggu di pick-up',
         ]);
-        if($requestPickup){
+        $updateStatusPemesanan->update([
+            'status_pemesanan' => 'Disetujui'
+        ]);
+        if($requestPickup && $updateStatusPemesanan){
             Session::flash('status', 'success');
             Session::flash('message', 'request pick up berhasil dibuat');
-            return redirect('/requestPickup');
+            return redirect('requestPickup/'.$id);
         } else {
             Session::flash('status', 'failed');
             Session::flash('message', 'request pick up gagal dibuat');
-            return redirect('/requestPickup');
+            return redirect('requestPickup/'.$id);
         }
     }
     public function insertProduct(Request $request){
@@ -64,40 +138,3 @@ class UserController extends Controller
             return redirect('/addproduct');
         }
     }
-    public function daftarproduct(Request $request){
-        $cari = $request->cari;
-        $listProduct = Product::where('nama_barang', 'LIKE', '%'. $cari. '%')
-        ->where('status_kelayakan', 'disetujui')
-        ->paginate(15);
-        return view('pages.DaftarProduct', compact(['listProduct']));
-    }
-    public function detailProduct($id){
-       $detailProduct = Product::find($id);
-        return view('pages.detailProduct', compact(['detailProduct']));
-    }
-    public function payments(Request $request){
-        $ekstensi = $request->file('bukti_pembayaran')->clientExtension();
-        $nama = $request->nama_pemesan.'-'.now()->timestamp.'.'.$ekstensi;
-        $request->file('bukti_pembayaran')->storeAs('images', $nama);
-
-        // Hitung total harga berdasarkan jumlah pemesanan
-        $total_harga = $request->harga * $request->jumlah_pemesan;
-
-        $payments = Transaction::create([
-            'nama_pemesan' => $request->nama_pemesan,
-            'email' => $request->email,
-            'no_telpon' => $request->no_telpon,
-            'produk_dibeli' => $request->produk_dibeli,
-            'jumlah_pemesan' => $request->jumlah_pemesan,
-            'harga' => $total_harga, // Gunakan total harga yang telah dihitung sebelumnya
-            'bukti_pembayaran' => $nama,
-            'status_pemesanan' => 'Pending',
-        ]);
-        if($payments){
-            return redirect('/sukses');
-        } 
-    }
-    public function sukses(){
-        return view('pages.sukses');
-    }
-}
